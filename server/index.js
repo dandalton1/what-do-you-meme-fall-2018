f1740d235bca82c7b7220b20b33adce920711621
Danny Dalton <daltond2@hawkmail.newpaltz.edu>
2018-10-15 11:18:15 -0400

    Updating to Moshe's commits before 10/15/18 I really need to keep track of my charger better

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
-const math = require("./math.js");
+const express = require('express');
+const game = require('./game/controller');
 
-console.log("Hello, world!");
-console.log("1 + 2 = " + math.add(1, 2));
+const app = express();
+
+const port = 3000;
+const server = "localhost";
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/", express.static(__dirname + "/../client/"));
+app.use('/game', game);
+
+app.listen(port);
+
+console.log(`listening on: http://${server}:${port}`);
