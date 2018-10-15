14a5cc3ee38dd5564cd1345b4ccb45d19f07d222
Danny Dalton <daltond2@hawkmail.newpaltz.edu>
2018-10-15 13:10:29 -0400

    Updated some stuff

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const game = require('./game/controller');
 
+console.log("loading...");
+
 const app = express();
 
-const port = 3000;
+const port = 80; // 80 is HTTP's default
 const server = "localhost";
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/", express.static(__dirname + "/../client/"));
-app.use('/game', game);
+app.use("/game", game);
 
 app.listen(port);
 
